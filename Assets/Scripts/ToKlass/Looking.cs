2ac6a7efc484354dccda0805496bce67a4e25828
Andrey <andrey-199213@mail.ru>
2021-05-10 20:53:00 +0300

    v0.0.1

---
+++ Assets/Scripts/ToKlass/Looking.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Looking : MonoBehaviour
+{
+    [Header("Looking angles")]
+    [SerializeField] private float minAngle;
+    [SerializeField] private float maxAngle;
+
+    [Header("Sensitivity")]
+    [SerializeField] private float sensitivity;
+
+    private float _angle = 0;
+
+    void Update()
+    {
+        _angle -= Input.GetAxis("Mouse Y") * sensitivity;
+        _angle = Mathf.Clamp(_angle, minAngle, maxAngle);
+
+        transform.localEulerAngles = new Vector3(_angle, 0);
+    }
+}
---
+++ Assets/Scripts/ToKlass/Moving.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    [Header("Movement config")]
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float rotateSpeed = 100f;
+
+    [Header("Reference")]
+    [SerializeField] private CharacterController characterController;
+
+    [Header("Gravity")]
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float graviScale;
+
+    private float gravity;
+    private bool jumping;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.forward * inputV + transform.right * inputH;
+        if (moveDirection.magnitude > 1) moveDirection.Normalize();
+
+        if (characterController.isGrounded)
+        {
+            jumping = Input.GetButton("Jump");
+            gravity = jumping ? jumpHeight : 0;
+        }
+        else
+        {
+            if (characterController.velocity.y == 0 && jumping) gravity = -0.01f;
+            else gravity += graviScale * Physics.gravity.y * Time.deltaTime;
+        }
+        moveDirection.y = gravity;
+
+        characterController.Move(moveDirection * Time.deltaTime * moveSpeed);
+        
+    }
+
+    private void Rotate()
+    {
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.up, mouseHorizontal * rotateSpeed * Time.deltaTime);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+}

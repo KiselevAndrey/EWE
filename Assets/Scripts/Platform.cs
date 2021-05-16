5f58acd4c5e0d74d72f32bf012521cd0224cb888
Andrey <andrey-199213@mail.ru>
2021-05-16 18:24:31 +0300

    continue to edit platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 
 public class Platform : MonoBehaviour
 {
+
 	[Header("Moving")]
 	[SerializeField] private bool canMove;
 	[SerializeField] private bool moveIsStart;
@@ -15,15 +16,20 @@ public class Platform : MonoBehaviour
 	[Header("Blinking")]
 	[SerializeField] private bool canBlink;
 	[SerializeField] private bool blinkIsStart;
-	[SerializeField] private float blinkTime;
+	[SerializeField] private float visibleTime;
+	[SerializeField] private float invisibleTime;
 	[SerializeField] private MeshRenderer meshRenderer;
 	[SerializeField] private Collider bodyCollider;
+	[SerializeField] private Collider triggerCollider;
 
 	//[Header("Additionaly Reference")]
-	//[SerializeField] private Collider triggerCollider;
+
+	private List<Transform> movingObjects;
 
 	private void Start()
 	{
+		movingObjects = new List<Transform>();
+
 		if(moveIsStart && canMove) Move(true);
 		if(blinkIsStart && canBlink) Blink(true);
 	}
@@ -40,33 +46,62 @@ public class Platform : MonoBehaviour
 		yield return new WaitForSeconds(stopTime);
 		Move(!moveUp);
 	}
+
+	private void AddToMovingList(Transform transform)
+    {
+		transform.parent = this.transform;
+		movingObjects.Add(transform);
+    }
+
+	private void DelAtMovingList(Transform transform)
+    {
+		transform.parent = null;
+		movingObjects.Remove(transform);
+    }
+
+	private void ClearMovingList()
+    {
+		if (movingObjects.Count == 0) return;
+
+		for (int i = 0; i < movingObjects.Count; i++)
+			movingObjects[i].parent = null;
+
+		movingObjects.Clear();
+    }
 	#endregion
 
 	#region Blinking
 	public void Blink(bool visible)
 	{
-		meshRenderer.enable(visible);
-		bodyCollider.enable(visible);
+		meshRenderer.enabled = visible;
+		bodyCollider.enabled = visible;
+		triggerCollider.enabled = visible;
+
+		if (!visible && canMove) ClearMovingList();
+
+		StartCoroutine(ChangeVisible(visible));
 	}
 
 	private IEnumerator ChangeVisible(bool visible)
 	{
-		yield return new WaitForSeconds(blinkTime);
-		Blinck(!visible);
+		yield return new WaitForSeconds(visible ? visibleTime : invisibleTime);
+		Blink(!visible);
 	}
 	#endregion
 
 	#region OnTrigger
 	private void OnTriggerEnter(Collider other)
 	{
-		if(other.gameObject.CompareTag("Player"))
-			other.gameObject.transform.parent = this.transform;
+		if(canMove)
+			if (other.gameObject.CompareTag("Player"))
+				AddToMovingList(other.transform);
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
-		if(other.gameObject.CompareTag("Player"))
-			other.gameObject.transform.parent = null;
+		if (canMove)
+			if (other.gameObject.CompareTag("Player"))
+				DelAtMovingList(other.transform);
 	}
 	#endregion
 }
